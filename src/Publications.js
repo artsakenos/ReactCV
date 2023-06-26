280856ed76143ba3906a8497d978729ff01dd9ec
Artsakenos <andrea.addis@gmail.com>
2023-06-26 05:56:56 +0200

    Initial commit

---
+++ src/Publications.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Publication from "./components/Publication";
+import publicationsData from "./CV_Publications.json";
+
+export default function Publications() {
+  return (
+    <div>
+      <h1>Publications</h1>
+      {publicationsData.map((publication) => (
+        <Publication key={publication.id} {...publication} />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/Publication.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Publication({
+  id,
+  authors,
+  title,
+  source,
+  sourceCoordinates,
+  publisher,
+  date,
+  links
+}) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>
+        <strong>Authors:</strong> {authors}
+      </p>
+      <p>
+        <strong>Source:</strong> {source}
+      </p>
+      <p>
+        <strong>Source Coordinates:</strong> {sourceCoordinates}
+      </p>
+      <p>
+        <strong>Publisher:</strong> {publisher}
+      </p>
+      <p>
+        <strong>Date:</strong> {date}
+      </p>
+      <p>
+        <strong>Links:</strong>
+      </p>
+      <ul>
+        {links &&
+          links.map((link, index) => (
+            <li key={index}>
+              <a href={link.url}>{link.name}</a>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
