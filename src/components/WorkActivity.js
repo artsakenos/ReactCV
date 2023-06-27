7e89bf177e69c89a33c92388da4dd340385e9731
Artsakenos <andrea.addis@gmail.com>
2023-06-27 11:24:15 +0200

    Inserzione automatica dei links di referenza!

--- src/components/WorkActivity.js
+++ src/components/WorkActivity.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { renderToString } from "react-dom/server";
+
+const replaceWorkActivityLinks_old = (description) => {
+  const linkPattern = /\[(\w+)\]/g;
+  return description.replace(linkPattern, (match, id) => {
+    const link = (
+      <Link key={id} to={`/publications?id=${id}`}>
+        {match}
+      </Link>
+    );
+    return link;
+  });
+};
+
+const replaceWorkActivityLinks = (description) => {
+  const linkPattern = /\[(\w+)\]/g;
+  return (
+    <>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: description.replace(linkPattern, (match, id) => {
+            return "<a href='/publications?id=" + match + "'>" + id + "</a>";
+          })
+        }}
+      ></div>
+    </>
+  );
+};
 
 function WorkActivity({ id, date, description, tags }) {
   return (
     <div>
       <h3>{id}</h3>
       {date && <p>Date: {date}</p>}
-      <p>Description: {description}</p>
+      <p>Description: {replaceWorkActivityLinks(description)}</p>
       <p>Tags: {tags.join(", ")}</p>
     </div>
   );
