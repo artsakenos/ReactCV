ddfda9db7b4b67dd3dc252c9625abebcb239ab4d
Andrea <andrea.addis@gmail.com>
2025-02-08 19:05:09 +0800

    Refactoring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-import App from "./App";
+import App from "./ur_apps/AppCV";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
